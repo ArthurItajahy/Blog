7da8e84ec518954ed22819a6d39d26c6b64931cc
emitpool <canalp3rry@gmail.com>
2022-02-21 18:33:48 -0300

    Try again

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -3,15 +3,20 @@ package com.generation.blogpessoal.model;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
@@ -28,14 +33,21 @@ public class Postagem {
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String titulo; 
 
-	@NotNull(message = "O atributo texto é Obrigatório!")
+	@NotBlank(message = "O atributo texto é Obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 500 caracteres")
 	private String texto;
 
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne(fetch = FetchType.EAGER, optional = true)
+	@JoinColumn(name = "tema_id")
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 
+	/* |==| Getters and Setters |==|*/
 	
 	public Long getId() {
 		return id;
@@ -68,5 +80,16 @@ public class Postagem {
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+	
+	
 
 }
