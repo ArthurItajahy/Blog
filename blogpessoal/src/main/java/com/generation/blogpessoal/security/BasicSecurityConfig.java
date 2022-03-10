6500ef3f6f5e5647c1ff96a8e7e39762475b8ecd
emitpool <canalp3rry@gmail.com>
2022-03-10 15:33:01 -0300

    Deploy no Heroku

--- blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.generation.blogpessoal.security;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -12,34 +13,50 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	
 	@Autowired
 	private UserDetailsService userDetailsService;
 
+	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		
+		
+		 auth.userDetailsService(userDetailsService);
 
-		auth.userDetailsService(userDetailsService);
-
-		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root"))
-				.authorities("ROLE_USER");
+		 auth.inMemoryAuthentication()
+			.withUser("root")
+			.password(passwordEncoder().encode("root"))
+			.authorities("ROLE_USER");
 
 	}
 
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Override
+	
+	 @Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 
-		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar")
-				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
-				.csrf().disable();
-
+		http.authorizeRequests()
+			.antMatchers("/usuarios/logar").permitAll()
+			.antMatchers("/usuarios/cadastrar").permitAll()
+			.antMatchers(HttpMethod.OPTIONS).permitAll()
+			.anyRequest().authenticated()
+			.and().httpBasic()
+			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().cors()
+			.and().csrf().disable();
+			
 	}
 }
